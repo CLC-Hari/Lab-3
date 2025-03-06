e70ae934cc3cf206f99f16650eb7dcf3d90aa11e
Harry-C <167039328+CLC-Hari@users.noreply.github.com>
2025-03-05 17:25:38 -0700

    Add files via upload

---
+++ todo list/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import ToDoList from './ToDoList';
+
+function App() {
+  // Part A: State Management
+  // Initialize state with hard-coded tasks
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
+  return (
+    <div className="App">
+      <h1>To-Do List App</h1>
+      {/* Part B: Passing Props */}
+      <ToDoList tasks={tasks} />
+    </div>
+  );
+}
+
+export default App;
